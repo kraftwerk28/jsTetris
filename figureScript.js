1f45b3d3b1e99acb2a4a87d91b28413423456774
kraftwerk28 <kefirchik3@gmail.com>
2017-11-02 23:55:42 +0200

    added main logic

--- figureScript.js
+++ figureScript.js
@@ -7,72 +7,68 @@ const allGrids = [
     [1, 1]
   ],
   [
-    [0, 1],
-    [0, 1],
-    [1, 1]
+    [0, 2],
+    [0, 2],
+    [2, 2]
   ],
   [
-    [1, 0],
-    [1, 1],
-    [1, 0]
+    [3, 0],
+    [3, 3],
+    [3, 0]
   ],
   [
-    [0, 1],
-    [1, 1],
-    [1, 0]
+    [0, 4],
+    [4, 4],
+    [4, 0]
   ],
   [
-    [1, 0],
-    [1, 1],
-    [0, 1]
+    [5, 0],
+    [5, 5],
+    [0, 5]
   ]
 ];
 
 const Figure = function (type, rot) {
-  this.fGrid = allGrids[Math.min(Math.max(type, 0), allGrids.length - 1)];
+  this.fGrid = allGrids[type];
   console.log(this.fGrid.length);
   this.fType = type;
   this.position = { x: 3, y: 0 };
   this.rotation = Math.min(Math.max(rot, 0), 2);
   this.rotate = () => {
+    let i = 0;
 
-    for (var x = 0; x < this.fGrid.length; x++) {
-      console.log(this.fGrid[x] + '\n');
+    for (i = 0; i < this.fGrid.length; i++) {
+      console.log(this.fGrid[i] + '\n');
     }
 
-
     let tempGrid = new Array(this.fGrid.length);
-    for (let i = 0; i < this.fGrid.length; i++) {
+    for (i = 0; i < this.fGrid.length; i++) {
       tempGrid[i] = this.fGrid[i].slice();
     }
     // console.log(this.fGrid.length + '   ' + this.fGrid[0].length);
     // console.log(tempGrid[0].length + '   ' + tempGrid.length);
 
     this.fGrid = new Array();
-    for (let i = 0; i < tempGrid[0].length; i++) {
+    for (i = 0; i < tempGrid[0].length; i++) {
       this.fGrid[i] = new Array(tempGrid.length);
     }
     // console.log(this.fGrid.length + ' ' + this.fGrid[0].length);
 
+    let x = 0;
+    let y = 0;
 
-    for (let y = 0; y < this.fGrid[0].length; y++) {
-      for (let x = 0; x < this.fGrid.length; x++) {
-        // this.fGrid[tempGrid[0].length - 1 - y, x] = tempGrid[x, y];
-        this.fGrid[x, y] = tempGrid[y, x] + 1;
+    for (y = 0; y < this.fGrid.length; y++) {
+      for (x = 0; x < this.fGrid[y].length; x++) {
+        this.fGrid[tempGrid[0].length - 1 - y][x] = tempGrid[x][y]; //counterclockwise rotate
+        // this.fGrid[y][tempGrid.length - 1 - x] = tempGrid[x][y]; //clockwise rotate
       }
     }
 
-    // for (var x = 0; x < tempGrid.length; x++) {
-    //   console.log(tempGrid[x] + '\n');
-    // }
-    // console.log('\n');
 
 
     Redraw();
-    // console.log(this.fGrid.length + ' ' + this.fGrid[0].length);
-  }
-  this.mvDown = () => {
-    this.position.y++;
-  }
-
+  };
+  this.mvDown = () => { this.position.y++; };
+  this.mvRight = () => { this.position.x++; };
+  this.mvLeft = () => { this.position.x--; };
 }
--- gameScript.js
+++ gameScript.js
@@ -10,20 +10,27 @@ let grid = new Array(20);
 for (let i = 0; i < 20; i++) {
   grid[i] = new Array(10);
 }
+for (let y = 0; y < grid.length; y++)
+  for (let x = 0; x < grid[0].length; x++)
+    grid[x][y] = 0;
 
 window.onload = () => {
   canvas = document.getElementById('canv');
   ctx = canvas.getContext('2d');
-  setInterval(Game, 500);
+  setInterval(Game, 200);
   document.addEventListener('keydown', keyDown);
   curFigure = new Figure(1, 1);
-  // alert(f);
-  // f.rotate();
-  // alert(f);
 }
 
 const Game = () => {
+  checkCol();
+  while (curFigure.position.x < 0)
+    curFigure.mvRight();
+  // while(curFigure.position.x<0)
+  // curFigure.mvRight();
   curFigure.mvDown();
+
+
   Redraw();
 }
 
@@ -54,44 +61,97 @@ const Redraw = () => {
         case 6:
           ctx.fillStyle = 'violet';
           break;
+        default:
+          ctx.fillStyle = 'black';
+          break;
       }
       ctx.fillRect(x * gs, y * gs, ts, ts);
     }
   }
-
   switch (curFigure.fType) {
-    case 1:
+    case 0:
       ctx.fillStyle = 'red';
       break;
-    case 2:
+    case 1:
       ctx.fillStyle = 'orange';
       break;
-    case 3:
+    case 2:
       ctx.fillStyle = 'yellow';
       break;
-    case 4:
+    case 3:
       ctx.fillStyle = 'lime';
       break;
-    case 5:
+    case 4:
       ctx.fillStyle = 'blue';
       break;
-    case 6:
+    case 5:
       ctx.fillStyle = 'violet';
+      break;
+    default:
+      ctx.fillStyle = 'black';
       break;
   }
   for (x = 0; x < curFigure.fGrid[0].length; x++) {
     for (y = 0; y < curFigure.fGrid.length; y++) {
-      if (curFigure.fGrid[x][y] != 0)
+      if (curFigure.fGrid[y][x] != 0)
         ctx.fillRect(curFigure.position.x * gs + x * gs, curFigure.position.y * gs + y * gs, ts, ts);
     }
   }
-  for (let i = 0; i < 200; i++) {
-    ctx.fillRect(i, i, 1, 1);
-  }
+
+  mxn.innerHTML = curFigure.fGrid[0].length + ' ' + curFigure.fGrid.length;
 }
 
 const Bake = () => {
+  let x = 0;
+  let y = 0;
+  for (x = 0; x < curFigure.fGrid[0].length; x++) {
+    for (y = 0; y < curFigure.fGrid.length; y++) {
+      if (curFigure.fGrid[y][x] !== 0)
+        grid[curFigure.position.x + x][curFigure.position.y + y] = curFigure.fGrid[y][x];
+    }
+  }
+  curFigure = new Figure(Math.floor(Math.random() * 4), Math.floor(Math.random() * 2));
+  // destroyRaw();
+}
 
+const checkCol = () => {
+  let x = 0;
+  let y = 0;
+  for (x = 0; x < curFigure.fGrid[0].length; x++) {
+    for (y = 0; y < curFigure.fGrid.length; y++) {
+      try {
+        if (grid[curFigure.position.x + x][curFigure.position.y + y + 1] !== 0 && curFigure.fGrid[y][x] !== 0) {
+          // console.log(grid[curFigure.position.x + x][curFigure.position.y + y] + '!!!');
+          Bake();
+          break;
+        }
+      } catch (error) {
+        while (curFigure.position.x < 0)
+          curFigure.mvRight();
+        while (curFigure.position.x > grid[0].length - curFigure[0].length - 10)
+          curFigure.mvLeft();
+      }
+    }
+  }
+}
+
+const destroyRaw = () => {
+  let raw = 0;
+  let cell = 0;
+  let c = 0;
+  for (raw = grid.length - 1; raw >= 0; raw--) {
+    for (cell = 0; cell < grid[raw].length; cell++) {
+      if (grid[raw][cell] !== 0) {
+        c++;
+      }
+    }
+    if (cell >= 8) {
+      grid.splice(raw, 1);
+    }
+    console.log(c);
+    c = 0;
+
+  }
 }
 
 const keyDown = (e) => {
@@ -99,8 +159,18 @@ const keyDown = (e) => {
     case 32:
       curFigure.rotate();
       break;
-
-    default:
+    case 37:
+    case 65:
+      curFigure.mvLeft();
+      Redraw();
+      break;
+    case 39:
+    case 68:
+      curFigure.mvRight();
+      Redraw();
+      break;
+    case 66:
+      Bake();
       break;
   }
 }
